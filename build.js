046a3861e1afebf90808780003e84fcbf9674830
Norman Rzepka <code@normanrz.com>
2021-02-22 09:45:06 +0100

    less version

--- build.js
+++ build.js
@@ -1,7 +1,17 @@
 const esbuild = require('esbuild');
+const { lessLoader } = require('esbuild-plugin-less');
+
 
 esbuild.build({
     entryPoints: ['index.js'],
     bundle: true,
     outfile: 'bundle.js',
+    loader: {
+      '.eot': 'file',
+      '.woff': 'file',
+      '.woff2': 'file',
+      '.svg': 'file',
+      '.ttf': 'file',
+    },
+    plugins: [lessLoader()]
 }).catch((e) => console.error(e.message));
