0ab96c299c02eaa2660300b87a208d8be64c2c2b
Norman Rzepka <code@normanrz.com>
2021-02-22 09:56:23 +0100

    fix build.js

--- build.js
+++ build.js
@@ -4,4 +4,11 @@ esbuild.build({
     entryPoints: ['index.js'],
     bundle: true,
     outfile: 'bundle.js',
+    loader: {
+      '.eot': 'file',
+      '.woff': 'file',
+      '.woff2': 'file',
+      '.svg': 'file',
+      '.ttf': 'file',
+    },
 }).catch((e) => console.error(e.message));
